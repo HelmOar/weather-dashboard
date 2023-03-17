f0d7f4bf4a68d9c7f514ebc486e70bcf104577ae
caroline <chelmore1@bigpond.com>
2023-03-17 16:00:03 +1100

    jquery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
-var currentTempEl = document.getElementById("temperature");
-var cityEl = document.getElementById("city");
-var cityName = document.getElementById("city-name");
+var currentTempEl = $("#temperature");
+var cityEl = $("#city");
+var cityName = $("#city-name");
 
 
 //assigning a unique API to a variable
@@ -9,16 +9,12 @@ var apiKey = "9f9f70b3b395ca9a8a718b7f8b260804";
 
 function getWeather (cityName) {
     //current weather request from open weather API
-        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}';
+        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}';
 
-        fetch(apiUrl)
-        .then(function (response) {
-        response.json();
-        })
-        .then(function (data) {
-            
-        displayTemperature(currentTempEl);
-        });
-    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(currentTempEl) {
+            console.log(currentTempEl);
 
 }
