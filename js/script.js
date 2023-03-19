cffce3234aa819dbb6dde00da3d7bf4b0ec3300e
caroline <chelmore1@bigpond.com>
2023-03-19 11:22:12 +1100

    working on passing data back to history

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ var iconEl = document.getElementById("weather-icon");
 // var cityName = getElementById("city-name");
 var humidityEl = document.getElementById("humidity");
 var windEl = document.getElementById("wind-speed");
-// var forecast = querySelector("forecast");
-// var currentTime = dayjs().format('dddd, MMM D, YYYY h:mm A');
+var clearEl = document.getElementById("clear-history");
+var forecastEls = document.querySelectorAll("forecast"); 
+var fivedayEl = document.getElementById("fiveday-header");
 
 //https://openweathermap.org/img/wn/10d@2x.png
 //assigning a unique API to a variable
@@ -27,10 +28,12 @@ function getWeather () {
         .then(function(data) {
             console.log(data);
 
+
             var history = JSON.parse(localStorage.getItem("history")) || [];
             history.push(data.name);
             localStorage.setItem("history", JSON.stringify(history));
 
+           
             cityNameEl.textContent = data.name;
             var link = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
             iconEl.setAttribute("src", link);
@@ -45,7 +48,7 @@ searchEl.addEventListener( "click", getWeather)
 
 function getForcast () {
    var cityForcast = cityEl.value; 
-    var forecastQueryURL = `https://api.openweathermap.org/data/2.5/forecast?id=${city}&units=metric&appid=${apiKey}`;
+   var forecastQueryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityForcast}&units=metric&appid=${apiKey}`;
     
     fetch(forecastQueryURL)
     .then(function(response) {
@@ -54,27 +57,52 @@ function getForcast () {
 
     .then(function(data) {
     console.log(data);
-          
-    });
+    }) ;
 }
+    
 searchEl.addEventListener( "click", getForcast)
 
-//  fivedayEl.classList.remove("d-none");
+ fivedayEl.classList.remove("d-none");
 
+// var forecastEls = document.querySelectorAll(".forecast");
+// for (var i = 0; i < forecastEls.length; i++) {
+//     forecastEls[i].innerHTML = "";
 
+ 
 
+//create buttons for 
+//
+var button = document.createElement("button");
 
 function renderHistory (){
     var history = JSON.parse(localStorage.getItem("history")) || [];
     for (var i = 0; i < history.length; i++) {
         var city = history[i];
-        var li = document.createElement("li");
-        li.textContent = city;
-        li.setAttribute("data-city", city);
-        li.setAttribute("class", "list-group-item");
-        document.getElementById("history").appendChild(li);
+        // var button = document.createElement("button");
+        button.textContent = city;
+        button.setAttribute("data-city", city);
+        button.setAttribute("class", "list-group-item");
+        document.getElementById("history").appendChild(button);
 
     }
     
 }
 renderHistory();
+
+button.addEventListener("click", function () {
+    EventTarget.value = button.textContent;
+    getWeather();
+
+})
+//event listener for button .. clicked run a function to get the nme of the button... 
+//event.target.value... get weather and pass in the name for the weather and update...
+
+
+// Clear History button
+clearEl.addEventListener("click", function () {
+    localStorage.clear();
+    searchHistory = [];
+    
+})
+renderHistory();
+
